7cbc2f907f95b46a35319dd390aee48ffc440059
AxelOBT <axelob1010@gmail.com>
2025-07-03 09:04:05 -0400

    creacion de proyecto backend

---
+++ backend/app/Models/Libro.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Libro extends Model
+{
+    use HasFactory;
+
+    protected $table = 'libros';
+
+    protected $fillable = ['titulo', 'anio_publicacion', 'autor_id'];
+
+    public function autor()
+    {
+        return $this->belongsTo(Autor::class, 'autor_id');
+    }
+}
